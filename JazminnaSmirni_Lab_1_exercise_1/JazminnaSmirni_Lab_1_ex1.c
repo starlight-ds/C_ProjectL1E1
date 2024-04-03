54e3f9f6c261216f3ab5861f9e049d68ab1853d5
starlight.ds <jazminna.smirni@gmail.com>
2024-04-03 13:17:20 -0700

    init

---
+++ JazminnaSmirni_Lab_1_exercise_1/JazminnaSmirni_Lab_1_ex1.c
@@ -0,0 +1,21 @@
+/*
+ * Lab_1_ex1.c
+ *
+ *  Created on: Jan 31, 2024
+ *      Author: jsmirni
+ */
+
+#include <stdio.h>
+#define PI 3.14159
+
+int
+main(void)
+{
+	double radius;
+	double area;
+	double circumf;
+	int num_circ;
+	char circ_name;
+
+	return 0;
+}
